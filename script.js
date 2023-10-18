768cb8fed353cbcfdb7308385f703174887728c6
Simolah <simo.lahdenne@aalto.fi>
2023-10-18 16:21:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const drawButton = document.getElementById("drawButton");
 const cardInner = document.getElementById("cardInner");
 const cardBack = document.getElementById("cardBack");
-
-let cardText = []; // Array to store custom card text
+const cardData = []; // Array to store card data
 
 function drawRandomCard() {
-    const randomIndex = Math.floor(Math.random() * cardText.length);
-    return cardText[randomIndex];
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex];
 }
 
-function flipCard() {
-    const randomCard = drawRandomCard();
-    cardBack.textContent = randomCard;
+function flipCard(card) {
+    cardBack.textContent = card.text;
     cardInner.classList.add("card-flip");
 }
 
-// Fetch the JSON data from the external file
 fetch("cards.json")
     .then(response => response.json())
     .then(data => {
-        cardText = data.cards; // Assign the custom text to the cardText array
-        init(); // Call the init function once the data is loaded
+        cardData.push(...data.cards); // Load card data from JSON
+        init();
     })
     .catch(error => console.error("Error loading card data:", error));
 
@@ -28,7 +25,8 @@ function init() {
     drawButton.addEventListener("click", () => {
         cardInner.classList.remove("card-flip");
         setTimeout(() => {
-            flipCard();
-        }, 500); // 500ms matches the CSS transition duration
+            const randomCard = drawRandomCard();
+            flipCard(randomCard);
+        }, 500);
     });
 }
