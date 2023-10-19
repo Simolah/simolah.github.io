f6df149f4a77fa86e053d43eafed23fe0b779a7a
Simolah <simo.lahdenne@aalto.fi>
2023-10-19 16:54:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function drawRandomCard() {
 }
 
 function flipCard(card) {
-    cardBack.textContent = card.text;
+	const cardText = `<h2>${card.headline}</h2><p>${card.text}</p>`;
+    cardBack.innerHTML = cardText;
     // Remove any existing category classes
     cardBack.classList.remove("category-A", "category-B", "category-C");
     // Add the category class based on the card's category
@@ -27,6 +28,7 @@ fetch("cards.json")
 
 function init() {
     drawButton.addEventListener("click", () => {
+		resetCard();
         cardInner.classList.remove("card-flip");
         setTimeout(() => {
             const randomCard = drawRandomCard();
@@ -34,3 +36,38 @@ function init() {
         }, 600);
     });
 }
+
+const sidebarLinks = document.querySelectorAll('.sidebar p');
+
+// Function to flip and show a card
+function flipAndShowCard(card) {
+    cardBack.innerHTML = card.text;
+    cardBack.classList.remove("category-D");
+    cardBack.classList.add("category-" + card.category);
+    cardInner.classList.add("card-flip");
+}
+
+sidebarLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const cardData = JSON.parse(link.getAttribute('data-card'));
+		cardInner.classList.remove("card-flip");
+        setTimeout(() => {
+            flipAndShowCard(cardData);
+        }, 600);
+    });
+});
+
+function resetCard() {
+    cardInner.classList.remove("card-flip");
+    setTimeout(() => {
+    cardBack.classList.remove("category-A", "category-B", "category-C", "category-D"); // Add more categories as needed
+	}, 600);
+}
+const homeLink = document.getElementById("homeLink");
+
+homeLink.addEventListener("click", () => {
+    resetCard();
+});
+
+
+
