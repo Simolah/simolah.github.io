355335a8612b414fd2aa4f78d58a0c9b2f7dbc83
Simolah <simo.lahdenne@aalto.fi>
2023-10-20 12:35:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const drawButton = document.getElementById("drawButton");
 const cardInner = document.getElementById("cardInner");
 const cardBack = document.getElementById("cardBack");
 const cardData = []; // Array to store card data
+let previousCardIndex = -1; // Initialize with an invalid index
 
 function drawRandomCard() {
-    const randomIndex = Math.floor(Math.random() * cardData.length);
+	let randomIndex;
+
+    do {
+        randomIndex = Math.floor(Math.random() * cardData.length);
+    } while (randomIndex === previousCardIndex); // Keep randomizing until a different card is drawn
+
+    previousCardIndex = randomIndex; // Update the previous card index
     return cardData[randomIndex];
 }
 
 function flipCard(card) {
-    cardBack.textContent = card.text;
+	const cardText = `<h2>${card.headline}</h2><p>${card.text}</p>`;
+    cardBack.innerHTML = cardText;
     // Remove any existing category classes
     cardBack.classList.remove("category-A", "category-B", "category-C");
     // Add the category class based on the card's category
